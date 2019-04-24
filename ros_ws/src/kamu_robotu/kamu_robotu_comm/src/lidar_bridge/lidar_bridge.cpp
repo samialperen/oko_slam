baf9111feb08f41243754a31c9fc9026b1069162
dorasprime <mustafa.kilinc@ieee.metu.edu.tr>
2019-04-24 20:05:34 +0300

    src file of the lidar bridge is changed for the FSM

--- ros_ws/src/kamu_robotu/kamu_robotu_comm/src/lidar_bridge/lidar_bridge.cpp
+++ ros_ws/src/kamu_robotu/kamu_robotu_comm/src/lidar_bridge/lidar_bridge.cpp
@@ -24,10 +24,12 @@
 #include <std_msgs/Empty.h>
 #include <string>
 #include <sensor_msgs/LaserScan.h>
+#include "okoserial.h"
 
 
 
 serial::Serial ser;
+std::string eol="~~~~";
 float w=0,x=0,y=0,z=0;
 uint8_t dummy_8 = 0;
 int16_t dummy_16 = 0;
@@ -36,53 +38,48 @@ int i = 0, f = 0;
 
 std::string readable;
 
-void write_callback(const std_msgs::String::ConstPtr& msg){
-    ROS_INFO_STREAM("Writing to serial port" << msg->data);
-    ser.write(msg->data);
+void write_callback(const std_msgs::String::ConstPtr& msg)
+{
+    	ROS_INFO_STREAM("Writing to serial port" << msg->data);
+	ser.write(msg->data);
 }
 
 int main (int argc, char** argv){
-    ros::init(argc, argv, "laser_scan_publisher");
-    //ros::NodeHandle nh;
+    	ros::init(argc, argv, "laser_scan_publisher");
 	ros::NodeHandle n;
-
-
 	ros::Publisher scan_pub = n.advertise<sensor_msgs::LaserScan>("scan",50);
 
-    //ros::Subscriber write_sub = nh.subscribe("write", 1000, write_callback);
-    //ros::Publisher read_pub = nh.advertise<std_msgs::String>("read", 1000);
-
-	unsigned int num_readings = 64;
-	double laser_frequency = 1; // this true for 64 readings
+	unsigned int num_readings = 136;
+	double laser_frequency = 4; // this true for 64 readings
 	double ranges[num_readings];
 	int count = 0 ;
 
 
-    try
-    {
-        ser.setPort("/dev/rfcomm0");
-        ser.setBaudrate(115200);
+    	try
+    	{
+       		ser.setPort("/dev/rfcomm0");
+        	ser.setBaudrate(115200);
 		serial::stopbits_t  stopbits;
 		stopbits = serial::stopbits_two;
 		ser.setStopbits(stopbits);
-        serial::Timeout to = serial::Timeout::simpleTimeout(1000);
-        ser.setTimeout(to);
-        ser.open();
-    }
-    catch (serial::IOException& e)
-    {
-        ROS_ERROR_STREAM("Unable to open port ");
-        return -1;
-    }
+        	serial::Timeout to = serial::Timeout::simpleTimeout(1000);
+        	ser.setTimeout(to);
+        	ser.open();
+    	}
+    	catch (serial::IOException& e)
+    	{
+        	ROS_ERROR_STREAM("Unable to open port ");
+        	return -1;
+    	}
 
-    if(ser.isOpen()){
-        ROS_INFO_STREAM("Serial Port initialized");
-        ser.flush();
-    }else{
-        return -1;
-    }
+    	if(ser.isOpen()){
+        	ROS_INFO_STREAM("Serial Port initialized");
+        	ser.flush();
+    	}else{
+        	return -1;
+    	}
 
-    ros::Rate loop_rate(50);
+    	ros::Rate loop_rate(50);
 	//populate the laser scan message
 
 	sensor_msgs::LaserScan scan;
@@ -94,91 +91,46 @@ int main (int argc, char** argv){
 	scan.time_increment = (1/laser_frequency)/(num_readings);
 	scan.range_min = 0.092;
 	scan.range_max = 3.0;
-    while(n.ok()){
+    	while(n.ok()){
 
-        ros::spinOnce();
+        	ros::spinOnce();
 
-        if(ser.available()){
-            ROS_INFO_STREAM("Reading from serial port");
-            std_msgs::String result;
-            readable = ser.read(1);
+        	if(ser.available())
+		{
+            		ROS_INFO_STREAM("Reading from serial port");
+            		std_msgs::String result;
+            		readable = ser.readline(150,eol);
+			for(i=0;i<num_readings;i++)
+			{
+				dummy_8 = readable[i];
+				getSingleByte(dummy_8);
+				//ROS_INFO("DATA.%d : %x " , i , dummy_8);
+				//ROS_INFO("laserscan_mess.%d :%d " , k, laserscan_mess.data[k]);
+			}
+				
+		
+			scan.ranges.resize(64);
+			for(f = 0 ; f<64;f++)
+			{
 
+				scan.ranges[f] = laserscan_mess.data[f]/1000.0;
 
-				if(readable[0]==119) {
-					for(i=0;i<num_readings;i++){
-
-						readable = ser.read(2);
-						dummy_8 = readable[0];
-						dummy_16 = (readable[1]<<8|dummy_8);
-						ranges[i] = float(dummy_16/1.0);
-						ROS_INFO_STREAM(ranges[i]);
-
-					}
-					scan.ranges.resize(num_readings);
-	                for(f = 0 ; f<num_readings;f++){
-
-		                scan.ranges[f] = ranges[f]/1000;
-
-	                }
-	                f=0;
-					ros::Time scan_time = ros::Time::now();
-					scan.header.stamp = scan_time;
-					scan_pub.publish(scan);
-				}
+			}
+			f=0;
+			ros::Time scan_time = ros::Time::now();
+			scan.header.stamp = scan_time;
+			scan_pub.publish(scan);
+	
 
 			i=0;
-			//readable = ser.readline(1);
-        	 //   ROS_INFO_STREAM(range);
-            //read_pub.publish(readable[0]);
-        }
+			
+        	}
 
 		loop_rate.sleep();
 
 
-    }
+    	}
 }
 
 
-// IMU part
-
-/*
-	if(readable[0]==119) {
-	readable = ser.readline(2);
-	dummy_8 = readable[1];
-	dummy_16 = (readable[0]<<8|dummy_8);
-	w = float(dummy_16/10.0);
-	ROS_INFO_STREAM(w);
-	readable = ser.readline(1);
-	}
-
-
-	if(readable[0]==120) {
-	readable = ser.readline(2);
-	dummy_8 = readable[1];
-	dummy_16 = (readable[0]<<8|dummy_8);
-	x = float(dummy_16/10.0);
-	ROS_INFO_STREAM(x);
-	readable = ser.readline(1);
-	}
-
-
-	if(readable[0]==121) {
-	readable = ser.readline(2);
-	dummy_8 = readable[1];
-	dummy_16 = (readable[0]<<8|dummy_8);
-	y = float(dummy_16/10.0);
-	ROS_INFO_STREAM(y);
-	readable = ser.readline(1);
-	}
-
-
-	if(readable[0]==122) {
-	readable = ser.readline(2);
-	dummy_8 = readable[1];
-	dummy_16 = (readable[0]<<8|dummy_8);
-	z = float(dummy_16/10.0);
-	ROS_INFO_STREAM(z);
-	}
-
-	*/
 
